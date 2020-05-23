6fa4b94cfa9cc715a0c7876c0db4a691e3c615e5
yangwj <yangwenjie-u@outlook.com>
2020-05-23 20:41:51 +0800

    1

--- MaterialEvaluationCal/MaterialEvaluationCal.Calculates/CalculatesNew/qsf/test.cs
+++ MaterialEvaluationCal/MaterialEvaluationCal.Calculates/CalculatesNew/qsf/test.cs
@@ -10,43 +10,43 @@ namespace Calculates.CalculatesNew.qsf
     {
         public void T()
         {
-            string jcxm = "、" + dic["jcxm"].Replace(',', '、') + "、";
-            if (jcxm.Contains("、厚度、"))
-            {
-                dic["hd"] = Round(GetSafeDouble(dic["schd1"]) + GetSafeDouble(dic["schd2"]) + GetSafeDouble(dic["schd3"]) + GetSafeDouble(dic["schd4"])) / 4,0).ToString();
-            }
-            else
-            {
-                dic["hd"] = "----";
-            }
+            //string jcxm = "、" + dic["jcxm"].Replace(',', '、') + "、";
+            //if (jcxm.Contains("、厚度、"))
+            //{
+            //    dic["hd"] = Round(GetSafeDouble(dic["schd1"]) + GetSafeDouble(dic["schd2"]) + GetSafeDouble(dic["schd3"]) + GetSafeDouble(dic["schd4"])) / 4,0).ToString();
+            //}
+            //else
+            //{
+            //    dic["hd"] = "----";
+            //}
 
-            if (jcxm.Contains("、密度（表干法、水中法、蜡封法）、"))
-            {
-                if (dic["syff"] == "表干法" || dic["syff"] == "水中重法")
-                {
-                    dic["ysd"] = Round(GetSafeDouble(dic["kqzzl"]) / (GetSafeDouble(dic["bgzl"]) - GetSafeDouble(dic["szzl"])) * 0.9771, 3).ToString();
-                }
-                else if (dic["syff"] == "蜡封法")
-                {
-                    if (dic["sfhs"] == "是")   //用滑石粉
-                    {
-                        dic["ysd"] = Round(GetSafeDouble(dic["sjkz"]) / ((GetSafeDouble(dic["lkzl"]) - GetSafeDouble(dic["lszl"])) -[(GetSafeDouble(dic["lkzl"]) - GetSafeDouble(dic["thkz"])) / GetSafeDouble(dic["slmd"]) + (GetSafeDouble(dic["thkz"]) - GetSafeDouble(dic["sjkz"])) / GetSafeDouble(dic["hsmd"])]) * 0.9771, 3).ToString();
-                    }
-                    else
-                    {
-                        dic["ysd"] = Round(GetSafeDouble(dic["sjkz"]) / ((GetSafeDouble(dic["lkzl"]) - GetSafeDouble(dic["lszl"])) - (GetSafeDouble(dic["lkzl"]) - GetSafeDouble(dic["sjkz"])) / GetSafeDouble(dic["slmd"])) * 0.9771, 3).ToString();
-                    }
-                }
-                else
-                {
-                    dic["syff"] = "----";
-                }
+            //if (jcxm.Contains("、密度（表干法、水中法、蜡封法）、"))
+            //{
+            //    if (dic["syff"] == "表干法" || dic["syff"] == "水中重法")
+            //    {
+            //        dic["ysd"] = Round(GetSafeDouble(dic["kqzzl"]) / (GetSafeDouble(dic["bgzl"]) - GetSafeDouble(dic["szzl"])) * 0.9771, 3).ToString();
+            //    }
+            //    else if (dic["syff"] == "蜡封法")
+            //    {
+            //        if (dic["sfhs"] == "是")   //用滑石粉
+            //        {
+            //            dic["ysd"] = Round(GetSafeDouble(dic["sjkz"]) / ((GetSafeDouble(dic["lkzl"]) - GetSafeDouble(dic["lszl"])) -[(GetSafeDouble(dic["lkzl"]) - GetSafeDouble(dic["thkz"])) / GetSafeDouble(dic["slmd"]) + (GetSafeDouble(dic["thkz"]) - GetSafeDouble(dic["sjkz"])) / GetSafeDouble(dic["hsmd"])]) * 0.9771, 3).ToString();
+            //        }
+            //        else
+            //        {
+            //            dic["ysd"] = Round(GetSafeDouble(dic["sjkz"]) / ((GetSafeDouble(dic["lkzl"]) - GetSafeDouble(dic["lszl"])) - (GetSafeDouble(dic["lkzl"]) - GetSafeDouble(dic["sjkz"])) / GetSafeDouble(dic["slmd"])) * 0.9771, 3).ToString();
+            //        }
+            //    }
+            //    else
+            //    {
+            //        dic["syff"] = "----";
+            //    }
 
-            }
-            else
-            {
-                dic["syff"] = "----";
-            }
+            //}
+            //else
+            //{
+            //    dic["syff"] = "----";
+            //}
 
 
 
